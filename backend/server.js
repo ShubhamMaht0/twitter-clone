6d75b6f90d71fd891f56575f32d99bc7f3765ee6
Gamma <shubhammahto2033@gmail.com>
2025-06-30 16:53:01 +0530

    3 -payload limit updated

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-cloudinary.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-app.use(express.json());
+app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
